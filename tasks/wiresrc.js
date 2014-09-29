c3c7728e29bf847e58a899c6df63ae365411cde6
87040F <alex.heller@instinet.com>
2014-09-29 14:54:02 -0400

    small fix to excludes path

--- tasks/wiresrc.js
+++ tasks/wiresrc.js
@@ -29,15 +29,13 @@ function wiresrc(grunt) {
         appPath = config.cwd+'/' || 'app/';
 
         Object.keys(config.scope).forEach(function (type) {
-            // console.info(config.scope[type]);
             config.scope[type].includes.forEach(function (pattern) {
                 var paths = grunt.file.expand(path.join(appPath, pattern));
                 paths.forEach(function (path) {
                     var excluded;
                     if (config.scope[type].excludes) {
                         config.scope[type].excludes.forEach(function (excludePattern) {
-                            // console.info(path.substr(appPath.length), "/" + excludePattern);
-                            if (minimatch(path.substr(appPath.length), "/" + excludePattern)) {
+                            if (minimatch(path.substr(appPath.length), excludePattern)) {
                                 excluded = true;
                                 return;
                             }
